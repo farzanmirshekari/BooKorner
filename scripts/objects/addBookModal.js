6cc06fed5b4204f2e3d2a846790a9ca46fb41553
farzanmirshekari <83178446+farzanmirshekari@users.noreply.github.com>
2021-08-09 15:07:54 -0400

    Users can now input the book's ISBN to add it to their library.

--- scripts/objects/addBookModal.js
+++ scripts/objects/addBookModal.js
@@ -7,23 +7,6 @@ export default class AddBookModal {
             document.querySelector("#addBookModal"));
     }
 
-    getInputFields() {
-        return [
-            document.querySelector("#inputBookTitle"),
-            document.querySelector("#inputBookAuthor"),
-            document.querySelector("#inputBookPages"),
-        ];
-    }
-
-    resetForm() {
-        const inputs = this.getInputFields();
-        for(let i = 0; i<inputs.length; i++) {
-            inputs[i].value = "";
-            inputs[i].classList.remove("is-invalid");
-        }
-        document.querySelector("#inputBookReadStatus").checked = false;
-    }
-
     setUpCancelButton() {
         this.cancelButton = document.querySelector("#addBookModalCancelButton");
         this.cancelButton.addEventListener("click" , () => {
@@ -32,42 +15,57 @@ export default class AddBookModal {
     }
 
     isInputValid() {
-        const inputs = this.getInputFields();
-        let isValid = true;
-        for(let i = 0; i < inputs.length; i++) {
-            if(inputs[i].value == "") {
-                isValid = false;
-            }
+        const ISBNInputValue = document.querySelector("#inputBookISBN").value;
+        let isValid = false;
+        if(ISBNInputValue.length == 13 || ISBNInputValue.length == 10) {
+            isValid = true;
         }
         return isValid;
     }
 
-    createBookObject() {
-        return new Book(
-            this.main,
-            document.querySelector('#inputBookTitle').value,
-            document.querySelector('#inputBookAuthor').value,
-            document.querySelector('#inputBookPages').value,
-            document.querySelector('#inputBookReadStatus').checked,
-        );
-    }
-
     saveBook() {
-        const newBook = this.createBookObject(); 
-        newBook.display();
-        this.main.library.addBook(newBook);
+        let results = [];
+        const isbn = document.getElementById("inputBookISBN").value.toString();
+        const url = "https://www.googleapis.com/books/v1/volumes?q=isbn:" + isbn;
+        fetch(url)
+        .then(response => {
+            return response.json();
+        }).then(data => {
+            results[0] = data.items[0].volumeInfo.title;
+            results[1] = data.items[0].volumeInfo.authors[0];
+            results[2] = Number(data.items[0].volumeInfo.pageCount);
+            return results;
+        }).then(finalInfo => {
+            console.log(finalInfo);
+            console.log(finalInfo[0]);
+            console.log(finalInfo[1]);
+            console.log(finalInfo[2]);
+            return new Book(
+                this.main,
+                finalInfo[0],
+                finalInfo[1],
+                finalInfo[2],
+                document.querySelector('#inputBookReadStatus').checked,
+                );
+            
+        }).then(book => {
+            console.log(book);
+            book.display();
+            this.main.library.addBook(book);
+        }).catch(e => {
+            console.log("Error!");
+        })
+        document.querySelector("#inputBookISBN").value = "";
     }
 
     invalidateForm() {
-        const inputs = this.getInputFields();
-        for(let i = 0; i < inputs.length; i++) {
-            if(inputs[i].value == "") {
-                inputs[i].classList.add("is-invalid");
-            } else {
-                inputs[i].classList.remove("is-invalid");
-            }
-        }    
-    }
+        const input = document.getElementById("inputBookISPN");
+        if(input == "" || input.length != 10 || input.length != 13) {
+            inputs[i].classList.add("is-invalid");
+         } else {
+            inputs[i].classList.remove("is-invalid");
+         }
+    }    
 
     setUpSaveButton() {
         const saveButton = document.querySelector("#addBookModalSaveButton");
@@ -75,7 +73,6 @@ export default class AddBookModal {
             if(this.isInputValid()) {
                 this.saveBook();
                 this.modal.hide();
-                this.resetForm();
             } else {
                 this.invalidateForm();
             }
